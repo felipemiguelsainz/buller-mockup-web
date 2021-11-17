8bb32064471d0fc993acad138dc198110fe23c1a
felipemiguelsainz <felipemiguelsainz@gmail.com>
2021-11-17 13:53:36 -0300

    Pagina Web Buller

--- js/main.js
+++ js/main.js
@@ -3,5 +3,5 @@ img2 = document.getElementById("img2"),
 img3 = document.getElementById("img3"); 
 
 img1.onclick = () => {document.querySelector(".carrousel").style.transform = 'translateX(-0%)'}
-img2.onclick = () => {document.querySelector(".carrousel").style.transform = 'translateX(-84%)'}
-img3.onclick = () => {document.querySelector(".carrousel").style.transform = 'translateX(-165%)'}
+img2.onclick = () => {document.querySelector(".carrousel").style.transform = 'translateX(-70%)'}
+img3.onclick = () => {document.querySelector(".carrousel").style.transform = 'translateX(-140%)'}
